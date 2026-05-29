7f28355086910c7aad367999a81724e2cea072ec
Magic_Bili <124496967+addpromax@users.noreply.github.com>
2026-05-29 10:19:03 +0800

    支持最新版本 26.2 （此版本不再支持1.12.2以下）

--- src/main/java/cn/wekyjay/www/wkkit/tool/items/Barrier.java
+++ src/main/java/cn/wekyjay/www/wkkit/tool/items/Barrier.java
@@ -1,20 +1,18 @@
 package cn.wekyjay.www.wkkit.tool.items;
 
-import cn.wekyjay.www.wkkit.tool.WKTool;
-import de.tr7zw.changeme.nbtapi.NBT;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public enum Barrier {
-		DEFAULT();
-		private ItemStack item;
-		Barrier() {
-			if(WKTool.getVersion() <= 7) {
-				item = NBT.itemStackFromNBT(NBT.parseNBT("{id:30s,Count:1b,Damage:0s,}"));
-			}else {
-				item = new org.bukkit.inventory.ItemStack(org.bukkit.Material.BARRIER);
-			}
-		}
-		public ItemStack getItemStack() {
-			return item;
-		}
+	DEFAULT;
+	
+	private ItemStack item;
+	
+	Barrier() {
+		this.item = new ItemStack(Material.BARRIER, 1);
+	}
+	
+	public ItemStack getItemStack() {
+		return item.clone();
+	}
 }
--- src/main/java/cn/wekyjay/www/wkkit/tool/items/GlassPane.java
+++ src/main/java/cn/wekyjay/www/wkkit/tool/items/GlassPane.java
@@ -1,49 +1,35 @@
 package cn.wekyjay.www.wkkit.tool.items;
 
 import cn.wekyjay.www.wkkit.tool.WKTool;
-import de.tr7zw.changeme.nbtapi.NBT;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public enum GlassPane {
-	DEFAULT(),
-	WHITE("white","0s"),
-	ORANGE("orange","1s"),
-	MAGENTA("magenta","2s"),
-	LIGHT_BLUE("light_blue","3s"),
-	YELLOW("yellow","4s"),
-	LIME("lime","5s"),
-	PINK("pink","6s"),
-	GRAY("gray","7s"),
-	LIGHT_GRAY("light_gray","8s"),
-	CYAN("cyan","9s"),
-	PURPLE("purple","10s"),
-	BLUE("blue","11s"),
-	BROWN("brown","12s"),
-	GREEN("green","13s"),
-	RED("red","14s"),
-	BLACK("black","15s");
+	DEFAULT(Material.WHITE_STAINED_GLASS_PANE),
+	WHITE(Material.WHITE_STAINED_GLASS_PANE),
+	ORANGE(Material.ORANGE_STAINED_GLASS_PANE),
+	MAGENTA(Material.MAGENTA_STAINED_GLASS_PANE),
+	LIGHT_BLUE(Material.LIGHT_BLUE_STAINED_GLASS_PANE),
+	YELLOW(Material.YELLOW_STAINED_GLASS_PANE),
+	LIME(Material.LIME_STAINED_GLASS_PANE),
+	PINK(Material.PINK_STAINED_GLASS_PANE),
+	GRAY(Material.GRAY_STAINED_GLASS_PANE),
+	LIGHT_GRAY(Material.LIGHT_GRAY_STAINED_GLASS_PANE),
+	CYAN(Material.CYAN_STAINED_GLASS_PANE),
+	PURPLE(Material.PURPLE_STAINED_GLASS_PANE),
+	BLUE(Material.BLUE_STAINED_GLASS_PANE),
+	BROWN(Material.BROWN_STAINED_GLASS_PANE),
+	GREEN(Material.GREEN_STAINED_GLASS_PANE),
+	RED(Material.RED_STAINED_GLASS_PANE),
+	BLACK(Material.BLACK_STAINED_GLASS_PANE);
 
 	private ItemStack item;
-	GlassPane() {
-		int version = WKTool.getVersion();
-		if(version <= 12 && WKTool.getVersion() > 7) {
-			item = NBT.itemStackFromNBT(NBT.parseNBT("{id:\"minecraft:stained_glass_pane\",Count:1b,Damage:0s}"));
-		}else if(WKTool.getVersion() <= 7) {
-			item = NBT.itemStackFromNBT(NBT.parseNBT("{id:102s,Count:1b,Damage:0s,}"));
-		}else {
-			item = NBT.itemStackFromNBT(NBT.parseNBT("{id:\"white_stained_glass_pane\",Count:1b}"));
-		}
+	
+	GlassPane(Material material) {
+		this.item = new ItemStack(material, 1);
 	}
-	GlassPane(String color,String data) {
-		if(WKTool.getVersion() <= 12 && WKTool.getVersion() > 7) {
-			item = NBT.itemStackFromNBT(NBT.parseNBT("{id:\"minecraft:stained_glass_pane\",Count:1b,Damage:"+ data + "}"));
-		}else if(WKTool.getVersion() <= 7) {
-			item = NBT.itemStackFromNBT(NBT.parseNBT("{id:102s,Count:1b,Damage:0s,}"));
-		}else {
-			item = NBT.itemStackFromNBT(NBT.parseNBT("{id:\""+ color +"_stained_glass_pane\",Count:1b}"));
-		}
-	}
+	
 	public ItemStack getItemStack() {
-		return item;
+		return item.clone();
 	}
 }
